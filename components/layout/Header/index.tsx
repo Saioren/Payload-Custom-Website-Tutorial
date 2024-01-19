9d1a10f4512198e7909e129c6846a22fe5682711
Evan Daniel Mikrut <mikrutevan@gmail.com>
2024-01-18 21:41:51 -0500

    up til nonfunctional menu
    20:45 in video, everything is in place but the menu button doesnt do anything yet

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import classes from "./index.module.scss";
+import Icon from "../../Graphics/Icon";
+import Link from "next/link";
+import Hamburger from "./Hamburger";
+import { useModal } from "@faceless-ui/modal";
+
+const menuSlug = "menu";
 
 export const Header: React.FC = () => {
   return (
     <header className={classes.header}>
-      <div>Logo</div>
-      <div>Menu</div>
+      <Link href="/" className={classes.logo}>
+        <Icon className={classes.icon} />
+      </Link>
+      <button type="button" className={classes.menuButton}>
+        <Hamburger active={true} />
+      </button>
     </header>
   );
 };
