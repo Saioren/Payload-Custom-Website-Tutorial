76b622489ca27e058257c7d85968eda1ec832927
Evan Daniel Mikrut <mikrutevan@gmail.com>
2024-01-19 18:39:23 -0500

    fixed modal fetch
    linked mega menu with type export, made type.ts for header to be the middle man, imported megamenu and stuff from types into header. then imported types from the api in to the actual app. defined the types, and threw them as props into my header, footer, and component. then copy pasted the logic for fetching the pages at the bottom of the screen, i dont get what that block does yet but maybe soon

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -3,16 +3,26 @@ import classes from "./index.module.scss";
 import Icon from "../../Graphics/Icon";
 import Link from "next/link";
 import Hamburger from "./Hamburger";
-import { useModal } from "@faceless-ui/modal";
-import { useState, useRef } from "react";
+import { Modal, useModal } from "@faceless-ui/modal";
+import { useState } from "react";
+import GridContainer from "../GridContainer";
+import { Grid, Cell } from "@faceless-ui/css-grid";
+import { Props } from "./types";
 
 const menuSlug = "menu";
 
-export const Header: React.FC = () => {
+export const Header: React.FC<Props> = ({ megaMenu, socialMedia }) => {
+  console.log("megaMenu:", megaMenu);
   const [isMenuActive, setMenuActive] = useState(false);
+  const { toggleModal } = useModal();
 
   const toggleMenu = () => {
     setMenuActive(!isMenuActive);
+  };
+
+  const handleToggle = () => {
+    toggleMenu();
+    toggleModal("my-modal");
   };
 
   return (
@@ -20,9 +30,24 @@ export const Header: React.FC = () => {
       <Link href="/" className={classes.logo}>
         <Icon className={classes.icon} />
       </Link>
-      <button onClick={toggleMenu} type="button" className={classes.menuButton}>
+      <button
+        onClick={handleToggle}
+        type="button"
+        className={classes.menuButton}
+      >
         <Hamburger active={isMenuActive} />
       </button>
+      <Modal slug="my-modal">
+        <GridContainer>
+          <Grid>
+            <Cell cols={8} htmlElement="nav">
+              {megaMenu?.nav?.map(({ link }, i) => (
+                <h3 key={i}>{link.label}</h3>
+              ))}
+            </Cell>
+          </Grid>
+        </GridContainer>
+      </Modal>
     </header>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,81 @@
 import React from "react";
-import type { AppProps } from "next/app";
-import { ModalProvider } from "@faceless-ui/modal";
+import { ModalContainer, ModalProvider } from "@faceless-ui/modal";
 import { GridProvider } from "@faceless-ui/css-grid";
 import { Header } from "../components/layout/Header";
 import { Footer } from "../components/layout/Footer";
+import App from "next/app";
+import { Type } from "../collections/Page";
+import { Type as MegaMenuType } from "../globals/MegaMenu";
+import { Type as FooterType } from "../globals/Footer";
+import { Type as SocialMediaType } from "../globals/SocialMedia";
+
+type AppProps = {
+  pageProps: object;
+  Component: React.FC<{ footer: FooterType; socialMedia: SocialMediaType }>;
+} & {
+  megaMenu: MegaMenuType;
+  footer: FooterType;
+  socialMedia: SocialMediaType;
+};
 
 import classes from "../css/style.css";
 
-const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
-  <ModalProvider zIndex={30}>
-    <GridProvider
-      rowGap={{
-        s: "2",
-        m: "2",
-        l: "2",
-        xl: "2",
-      }}
-      colGap={{
-        s: "2",
-        m: "2",
-        l: "2",
-        xl: "2",
-      }}
-      cols={{
-        s: 8,
-        m: 8,
-        l: 12,
-        xl: 12,
-      }}
-    >
-      <div className={classes.app}>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-    </GridProvider>
-  </ModalProvider>
-);
+const MyApp = (appProps: AppProps): React.ReactElement => {
+  const { Component, pageProps, megaMenu, footer, socialMedia } = appProps;
+
+  return (
+    <ModalProvider transTime={250} zIndex={20}>
+      <GridProvider
+        rowGap={{
+          s: "2",
+          m: "2",
+          l: "2",
+          xl: "2",
+        }}
+        colGap={{
+          s: "2",
+          m: "2",
+          l: "2",
+          xl: "2",
+        }}
+        cols={{
+          s: 8,
+          m: 8,
+          l: 12,
+          xl: 12,
+        }}
+      >
+        <div className={classes.app}>
+          <Header megaMenu={megaMenu} socialMedia={socialMedia} />
+          <Component {...pageProps} footer={footer} socialMedia={socialMedia} />
+        </div>
+      </GridProvider>
+      <ModalContainer />
+    </ModalProvider>
+  );
+};
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const [megaMenu, footer, socialMedia] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/globals/mega-menu`).then(
+      (res) => res.json()
+    ),
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/globals/footer`).then(
+      (res) => res.json()
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/globals/social-media`
+    ).then((res) => res.json()),
+  ]);
+
+  return {
+    ...appProps,
+    megaMenu,
+    footer,
+    socialMedia,
+  };
+};
 
 export default MyApp;
